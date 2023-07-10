a960bb26ae8f3822256d316777f373de9bc323ee
namtran6701 <nam3864779@gmail.com>
2023-07-09 23:45:29 -0400

    more code for file_1

--- file_1.py
+++ file_1.py
@@ -3,3 +3,9 @@ print("hehllow world")
 # create a simple loop to print 1 to 10 
 for i in range(1,11):
     print(i)
+
+
+# file_2 will create a loop to print 11 to 20. 
+
+for i in range(11, 21):
+    print(i)
